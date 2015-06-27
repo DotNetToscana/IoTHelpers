77d9710aa6f01352165052f063129f33329ce7b4
Marco Minerva <marco.minerva@gmail.com>
2015-06-27 16:21:54 +0200

    Added the SR 501 PIR Motion Detector.

---
+++ IoTHelpers/Gpio/Modules/Sr501PirMotionDetector.cs
@@ -0,0 +1,61 @@
+﻿using IoTHelpers.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+
+namespace IoTHelpers.Gpio.Modules
+{
+    public class Sr501PirMotionDetector : GpioModule
+    {
+        private GpioPinValue lastPinValue;
+
+        public event EventHandler MotionDetected;
+        public event EventHandler MotionStoppped;
+
+        private bool IsMotionDetected { get; set; }
+
+        public bool RaiseEventsOnUIThread { get; set; } = false;
+
+        public Sr501PirMotionDetector(int pinNumber) : base(pinNumber, GpioPinDriveMode.Input, LogicValue.Positive)
+        {
+            lastPinValue = ActualLowPinValue;
+
+            Pin.ValueChanged += Pin_ValueChanged;
+        }
+
+        private void Pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            var currentPinValue = Pin.Read();
+            //System.Diagnostics.Debug.WriteLine(currentPinValue);
+
+            // If same value of last read, exits. 
+            if (currentPinValue == lastPinValue)
+                return;
+
+            // Checks the pin value.
+            if (currentPinValue == ActualHighPinValue)
+            {
+                IsMotionDetected = true; 
+                RaiseEventHelper.CheckRaiseEventOnUIThread(this, MotionDetected, RaiseEventsOnUIThread);
+            }
+            else if (currentPinValue == ActualLowPinValue)
+            {
+                IsMotionDetected = true;
+                RaiseEventHelper.CheckRaiseEventOnUIThread(this, MotionStoppped, RaiseEventsOnUIThread);
+            }
+
+            lastPinValue = currentPinValue;
+        }
+
+        public override void Dispose()
+        {
+            Pin.ValueChanged -= Pin_ValueChanged;
+            base.Dispose();
+        }
+    }
+}
