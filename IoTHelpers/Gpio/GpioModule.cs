1bd8b203a95dc1de9f81407b9a07ebeddeb54b63
Marco Minerva <marco.minerva@gmail.com>
2015-07-19 16:41:29 +0200

    Corrected tab spacing.

--- IoTHelpers/Gpio/GpioModule.cs
+++ IoTHelpers/Gpio/GpioModule.cs
@@ -7,64 +7,64 @@ using Windows.Devices.Gpio;
 
 namespace IoTHelpers.Gpio
 {
-	public enum LogicValue
-	{
-		Positive,
-		Negative
-	}
+    public enum LogicValue
+    {
+        Positive,
+        Negative
+    }
 
-	public class GpioModule : GpioModuleBase
-	{
-		public GpioPinValue ActualLowPinValue { get; set; }
+    public class GpioModule : GpioModuleBase
+    {
+        public GpioPinValue ActualLowPinValue { get; set; }
 
-		public GpioPinValue ActualHighPinValue { get; set; }
+        public GpioPinValue ActualHighPinValue { get; set; }
 
-		public GpioPin Pin { get; private set; }
+        public GpioPin Pin { get; private set; }
 
-		public GpioModule()
-			: base(GpioController.GetDefault())
-		{ }
+        public GpioModule()
+            : base(GpioController.GetDefault())
+        { }
 
-		public GpioModule(GpioController controller)
-			: base(controller)
-		{ }
+        public GpioModule(GpioController controller)
+            : base(controller)
+        { }
 
-		public GpioModule(int pinNumber, GpioPinDriveMode driveMode = GpioPinDriveMode.Input, LogicValue logicValue = LogicValue.Positive)
-			: this(GpioController.GetDefault(), pinNumber, driveMode, logicValue)
-		{ }
+        public GpioModule(int pinNumber, GpioPinDriveMode driveMode = GpioPinDriveMode.Input, LogicValue logicValue = LogicValue.Positive)
+            : this(GpioController.GetDefault(), pinNumber, driveMode, logicValue)
+        { }
 
-		public GpioModule(GpioController controller, int pinNumber, GpioPinDriveMode driveMode = GpioPinDriveMode.Input, LogicValue logicValue = LogicValue.Positive)
-			: base(controller)
-		{
-			Pin = Controller.OpenPin(pinNumber);
+        public GpioModule(GpioController controller, int pinNumber, GpioPinDriveMode driveMode = GpioPinDriveMode.Input, LogicValue logicValue = LogicValue.Positive)
+            : base(controller)
+        {
+            Pin = Controller.OpenPin(pinNumber);
 
-			// Shows an error if the pin wasn't initialized properly
-			if (Pin == null)
-				throw new ArgumentException($"There were problems initializing the GPIO {GetType().Name} pin.");
+            // Shows an error if the pin wasn't initialized properly
+            if (Pin == null)
+                throw new ArgumentException($"There were problems initializing the GPIO {GetType().Name} pin.");
 
-			if (logicValue == LogicValue.Positive)
-			{
-				ActualHighPinValue = GpioPinValue.High;
-				ActualLowPinValue = GpioPinValue.Low;
-			}
-			else
-			{
-				ActualHighPinValue = GpioPinValue.Low;
-				ActualLowPinValue = GpioPinValue.High;
-			}
+            if (logicValue == LogicValue.Positive)
+            {
+                ActualHighPinValue = GpioPinValue.High;
+                ActualLowPinValue = GpioPinValue.Low;
+            }
+            else
+            {
+                ActualHighPinValue = GpioPinValue.Low;
+                ActualLowPinValue = GpioPinValue.High;
+            }
 
-			Pin.SetDriveMode(driveMode);
-		}
+            Pin.SetDriveMode(driveMode);
+        }
 
-		public override void Dispose()
-		{
-			if (Pin != null)
-			{
-				Pin.Dispose();
-				Pin = null;
-			}
+        public override void Dispose()
+        {
+            if (Pin != null)
+            {
+                Pin.Dispose();
+                Pin = null;
+            }
 
-			base.Dispose();
-		}
-	}
+            base.Dispose();
+        }
+    }
 }
--- IoTHelpers/I2c/I2cDeviceBase.cs
+++ IoTHelpers/I2c/I2cDeviceBase.cs
@@ -8,44 +8,44 @@ using Windows.Devices.I2c;
 
 namespace IoTHelpers.I2c
 {
-	public class I2cDeviceBase : IDisposable
-	{
-		// I2C Controller name
-		public const string I2C_CONTROLLER_NAME = "I2C1";
+    public class I2cDeviceBase : IDisposable
+    {
+        // I2C Controller name
+        public const string I2C_CONTROLLER_NAME = "I2C1";
 
-		public I2cConnectionSettings Settings { get; }
+        public I2cConnectionSettings Settings { get; }
 
-		// I2C Device
-		public I2cDevice Device { get; private set; }
+        // I2C Device
+        public I2cDevice Device { get; private set; }
 
-		private string deviceSelector;
+        private string deviceSelector;
 
-		public I2cDeviceBase(int slaveAddress, I2cBusSpeed busSpeed = I2cBusSpeed.FastMode, I2cSharingMode sharingMode = I2cSharingMode.Shared, string i2cControllerName = I2C_CONTROLLER_NAME)
-		{
-			// Initialize I2C device
-			Settings = new I2cConnectionSettings(slaveAddress)
-			{
-				BusSpeed = busSpeed,
-				SharingMode = sharingMode
-			};
+        public I2cDeviceBase(int slaveAddress, I2cBusSpeed busSpeed = I2cBusSpeed.FastMode, I2cSharingMode sharingMode = I2cSharingMode.Shared, string i2cControllerName = I2C_CONTROLLER_NAME)
+        {
+            // Initialize I2C device
+            Settings = new I2cConnectionSettings(slaveAddress)
+            {
+                BusSpeed = busSpeed,
+                SharingMode = sharingMode
+            };
 
-			deviceSelector = I2cDevice.GetDeviceSelector(i2cControllerName);    /* Find the selector string for the I2C bus controller */
-		}
+            deviceSelector = I2cDevice.GetDeviceSelector(i2cControllerName);    /* Find the selector string for the I2C bus controller */
+        }
 
-		public async Task InitializeAsync()
-		{
-			var dis = await DeviceInformation.FindAllAsync(deviceSelector);     /* Find the I2C bus controller device with our selector string  */
-			Device = await I2cDevice.FromIdAsync(dis[0].Id, Settings);          /* Create an I2cDevice with our selected bus controller and I2C settings */
+        public async Task InitializeAsync()
+        {
+            var dis = await DeviceInformation.FindAllAsync(deviceSelector);     /* Find the I2C bus controller device with our selector string  */
+            Device = await I2cDevice.FromIdAsync(dis[0].Id, Settings);          /* Create an I2cDevice with our selected bus controller and I2C settings */
 
-			await InitializeSensorAsync();
-		}
+            await InitializeSensorAsync();
+        }
 
-		protected virtual Task InitializeSensorAsync() => Task.FromResult<object>(null);
+        protected virtual Task InitializeSensorAsync() => Task.FromResult<object>(null);
 
         public virtual void Dispose()
-		{
-			if (Device != null)
-				Device.Dispose();
-		}
-	}
+        {
+            if (Device != null)
+                Device.Dispose();
+        }
+    }
 }
