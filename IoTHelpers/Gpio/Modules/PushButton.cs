d2640b03af8bd91c29e86534425aeaf267040b41
Marco Minerva <marco.minerva@gmail.com>
2015-07-19 15:34:44 +0200

    Corrected tabs in PushButton.cs

--- IoTHelpers/Gpio/Modules/PushButton.cs
+++ IoTHelpers/Gpio/Modules/PushButton.cs
@@ -12,62 +12,62 @@ using Windows.UI.Xaml;
 
 namespace IoTHelpers.Gpio.Modules
 {
-	public enum ButtonType
-	{
-		PullDown = 0,
-		PullUp = 1
-	}
+    public enum ButtonType
+    {
+        PullDown = 0,
+        PullUp = 1
+    }
 
-	public class PushButton : GpioModule
-	{
-		private GpioPinValue lastPinValue;
+    public class PushButton : GpioModule
+    {
+        private GpioPinValue lastPinValue;
 
-		public bool IsPressed { get; private set; } = false;
+        public bool IsPressed { get; private set; } = false;
 
-		public bool RaiseEventsOnUIThread { get; set; } = false;
+        public bool RaiseEventsOnUIThread { get; set; } = false;
 
-		public event EventHandler Pressed;
-		public event EventHandler Released;
-		public event EventHandler Click;
+        public event EventHandler Pressed;
+        public event EventHandler Released;
+        public event EventHandler Click;
 
-		public PushButton(int pinNumber, ButtonType type = ButtonType.PullDown) : base(pinNumber, GpioPinDriveMode.Input, type == ButtonType.PullUp ? LogicValue.Positive : LogicValue.Negative)
-		{
-			lastPinValue = ActualLowPinValue;
+        public PushButton(int pinNumber, ButtonType type = ButtonType.PullDown) : base(pinNumber, GpioPinDriveMode.Input, type == ButtonType.PullUp ? LogicValue.Positive : LogicValue.Negative)
+        {
+            lastPinValue = ActualLowPinValue;
 
-			Pin.DebounceTimeout = TimeSpan.FromMilliseconds(20);
-			Pin.ValueChanged += Pin_ValueChanged;
-		}
+            Pin.DebounceTimeout = TimeSpan.FromMilliseconds(20);
+            Pin.ValueChanged += Pin_ValueChanged;
+        }
 
-		private void Pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
-		{
-			var currentPinValue = Pin.Read();
+        private void Pin_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            var currentPinValue = Pin.Read();
 
-			// If same value of last read, exits. 
-			if (currentPinValue == lastPinValue)
-				return;
+            // If same value of last read, exits. 
+            if (currentPinValue == lastPinValue)
+                return;
 
-			// Checks the pin value.
-			if (currentPinValue == ActualHighPinValue)
-			{
-				IsPressed = true;
-				RaiseEventHelper.CheckRaiseEventOnUIThread(this, Pressed, RaiseEventsOnUIThread);
-			}
-			else if (currentPinValue == ActualLowPinValue)
-			{
-				RaiseEventHelper.CheckRaiseEventOnUIThread(this, Released, RaiseEventsOnUIThread);
-				if (IsPressed)
-					RaiseEventHelper.CheckRaiseEventOnUIThread(this, Click, RaiseEventsOnUIThread);
+            // Checks the pin value.
+            if (currentPinValue == ActualHighPinValue)
+            {
+                IsPressed = true;
+                RaiseEventHelper.CheckRaiseEventOnUIThread(this, Pressed, RaiseEventsOnUIThread);
+            }
+            else if (currentPinValue == ActualLowPinValue)
+            {
+                RaiseEventHelper.CheckRaiseEventOnUIThread(this, Released, RaiseEventsOnUIThread);
+                if (IsPressed)
+                    RaiseEventHelper.CheckRaiseEventOnUIThread(this, Click, RaiseEventsOnUIThread);
 
-				IsPressed = false;
-			}
+                IsPressed = false;
+            }
 
-			lastPinValue = currentPinValue;
-		}
+            lastPinValue = currentPinValue;
+        }
 
-		public override void Dispose()
-		{
-			Pin.ValueChanged -= Pin_ValueChanged;
-			base.Dispose();
-		}
-	}
+        public override void Dispose()
+        {
+            Pin.ValueChanged -= Pin_ValueChanged;
+            base.Dispose();
+        }
+    }
 }
