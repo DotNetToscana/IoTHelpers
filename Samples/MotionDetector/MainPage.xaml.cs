133d9246060dbe19afb96a0420dbd9738e6ac90f
Marco Minerva <marco.minerva@gmail.com>
2015-08-05 18:26:03 +0200

    Updated IoTHelpers to Windows 10 IoT Core RTM (version 10.0.1024).

---
+++ Samples/MotionDetector/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using IoTHelpers.Boards;
+using IoTHelpers.Gpio.Modules;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MotionDetector
+{
+    public sealed partial class MainPage : Page
+    {
+        private MulticolorLed led;
+        private Sr501PirMotionDetector pir;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Unloaded += MainPage_Unloaded;
+
+            led = new MulticolorLed(redPinNumber: 27, greenPinNumber: 22, bluePinNumber: 23);
+
+            pir = new Sr501PirMotionDetector(5);
+            pir.RaiseEventsOnUIThread = true;
+            pir.MotionDetected += pir_MotionDetected;
+            pir.MotionStopped += pir_MotionStopped;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var board = RaspberryPiBoard.GetDefault();
+            if (board != null)
+            {
+                board.PowerLed.TurnOff();
+                board.StatusLed.TurnOn();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void pir_MotionDetected(object sender, EventArgs e)
+        {
+            // Starts the alarm.
+            led.TurnRed();
+            alarm.Play();
+        }
+
+        private void pir_MotionStopped(object sender, EventArgs e)
+        {
+            // Stops the alarm.
+            led.TurnOff();
+            alarm.Stop();
+        }
+
+        private void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            // Cleanup.
+            if (pir != null)
+            {
+                pir.MotionDetected -= pir_MotionDetected;
+                pir.MotionStopped -= pir_MotionStopped;
+                pir.Dispose();
+            }
+
+            if (led != null)
+                led.Dispose();
+        }
+    }
+}
---
+++ Samples/RemoteControl/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using Windows.UI;
+using RemoteControl.Models;
+using IoTHelpers.Gpio.Modules;
+using IoTHelpers.Boards;
+
+namespace RemoteControl
+{
+    public sealed partial class MainPage : Page
+    {
+        private MulticolorLed led;
+
+        private RemoteConnection connection;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Unloaded += MainPage_Unloaded;
+
+            connection = new RemoteConnection();
+            connection.OnLedEvent(LedEvent);
+
+            led = new MulticolorLed(redPinNumber: 27, greenPinNumber: 22, bluePinNumber: 23);
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await connection.ConnectAsync();
+
+            var board = RaspberryPiBoard.GetDefault();
+            if (board != null)
+            {
+                board.PowerLed.TurnOff();
+                board.StatusLed.TurnOn();
+            }
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void LedEvent(Rgb rgb)
+        {
+            led.Red = rgb.Red;
+            led.Green = rgb.Green;
+            led.Blue = rgb.Blue;
+        }
+
+        private void MainPage_Unloaded(object sender, object args)
+        {
+            // Cleanup
+            if (led != null)
+                led.Dispose();
+
+            if (connection != null)
+                connection.Dispose();
+        }
+    }
+}
